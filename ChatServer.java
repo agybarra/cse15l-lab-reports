dd41e769b67632204047dfeaafaca6bda215a8ec
agybarra <156254451+agybarra@users.noreply.github.com>
2024-01-28 14:02:42 -0800

    Update and rename Server.java to ChatServer.java

---
+++ ChatServer.java
@@ -0,0 +1,58 @@
+import java.io.IOException;
+import java.net.URI;
+import java.io.*;
+import java.util.*;
+
+class Handler implements URLHandler {
+    ArrayList<String> messages = new ArrayList<>();
+    
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            return "Please enter a message in the search bar";
+        } else if (url.getPath().equals("/add-message")) {
+            String[] parameters = url.getQuery().split("&");
+
+            String user = null;
+            String message = null;
+
+            for (String pair : parameters) {
+                String[] values = pair.split("=");
+                if (values.length == 2) {
+                    String key = values[0];
+                    String value = values[1];
+
+                    if ("s".equals(key)) {
+                        message = value;
+                    } else if ("user".equals(key)) {
+                        user = value;
+                    }
+                }
+               
+            }
+
+             if (message != null && user != null) {
+                String finalMessage = user + ": " + message;
+                messages.add(finalMessage);
+                return String.format("%s",String.join("\n", messages));
+        
+            } else {
+                return "Invalid parameters!";
+            }
+        }
+
+        return "404 Not Found!";
+    }
+}
+
+class ChatServer {
+    public static void main(String[] args) throws IOException {
+        if (args.length == 0) {
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
